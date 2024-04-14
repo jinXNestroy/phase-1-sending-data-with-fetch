ea7aff185d6361faf837ff67c29522c19c46bc89
DanielJoel <danpersie53@gmail.com>
2024-04-14 12:48:07 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName, email: userEmail
+        })
+
+
+    }).then(function (response) {
+        return response.json();
+    }).then(function (object) {
+        appendIdToDOM(object.id)
+    }).catch(function (error) {
+        const errorEl = document.createElement('p');
+        errorEl.textContent = error
+        document.body.appendChild(errorEl)
+    })
+}
+
+function appendIdToDOM(id) {
+    const idElement = document.createElement('p');
+    idElement.textContent = `New ID: ${id}`;
+    document.body.appendChild(idElement);
+}
